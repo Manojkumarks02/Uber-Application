4c30ace6781a65cef604d67a4aaa7ae9a87e4f74
Manojkumarks02 <manojkumarks063@gmail.com>
2024-11-07 11:45:03 +0530

    changes in RideService Impl

--- src/main/java/com/KT/project/uber/uberApp/exception/RuntimeConflictException.java
+++ src/main/java/com/KT/project/uber/uberApp/exception/RuntimeConflictException.java
@@ -1,10 +1,11 @@
 package com.KT.project.uber.uberApp.exception;
 
 public class RuntimeConflictException extends RuntimeException{
+
     public RuntimeConflictException() {
     }
+
     public RuntimeConflictException(String message) {
         super(message);
-
     }
 }
